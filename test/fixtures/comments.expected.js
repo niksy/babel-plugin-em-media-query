9c002eb3fcf3bcf0614811a03b1c1a88964d56a4
Ivan Nikolić <ivan.nikolic@trikoder.net>
2017-10-25 10:19:35 +0200

    Add processed media query ID to comments

--- test/fixtures/comments.expected.js
+++ test/fixtures/comments.expected.js
@@ -1,11 +1,11 @@
-var jackie = 'screen and (min-width:37.5em)';
+var jackie = /* @media _em-media-query-processed */'screen and (min-width:37.5em)';
 var lucy = {
-	winnie: 'screen and (min-width:37.5em)'
+	winnie: /* @media _em-media-query-processed */'screen and (min-width:37.5em)'
 };
 
-var daisy = 'screen and (min-width:37.5em)';
+var daisy = /* @media _em-media-query-processed */'screen and (min-width:37.5em)';
 var coco = {
-	sophie: 'screen and (min-width:37.5em)'
+	sophie: /* @media _em-media-query-processed */'screen and (min-width:37.5em)'
 };
 
-var jackie = /* jackie */'screen and (min-width:37.5em)';
+var jackie = /* @media _em-media-query-processed */ /* jackie */'screen and (min-width:37.5em)';
